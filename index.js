607094b216ddf9e79a60afa8ff4fa4e138b36f1f
Teodoro Garcia <tedtke514@gmail.com>
2022-03-09 17:07:10 -0600

    added some javascript loops for recursion

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+  
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
+  
+  function printReverseString(myString) {
+      console.log(myString[0]);
+
+      if (myString.length > 1) {
+          let mySubString = myString.substring(1, myString.reverse.length);
+          printReverseString(mySubString);
+      } else {
+          return true;
+      }
+  }
